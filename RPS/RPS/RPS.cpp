12dc78265620461feb78bbddf3de629ab926e4ac
Jakub Głatki <jakub.glatki@gmail.com>
2019-03-07 12:45:48 +0100

    RPS

--- RPS/RPS/RPS.cpp
+++ RPS/RPS/RPS.cpp
@@ -1,21 +1,58 @@
-﻿// RPS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <typeinfo>
 
-#include "pch.h"
-#include <iostream>
+class Kamien {
+};
+
+class Papier {
+};
+
+class Nozyczki {
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Kamien gracz1;
+
+	Papier gracz2;
+
+
+	if (typeid(gracz1) == typeid(Kamien)) {
+		if (typeid(gracz2) == typeid(Kamien)) {
+			std::cout << " Remis";
+		}
+		else if (typeid(gracz2) == typeid(Papier)) {
+			std::cout << " Gracz 2 wygrywa, papier bije kamien";
+		}
+		else if (typeid(gracz2) == typeid(Nozyczki)) {
+			std::cout << " Gracz 1 wygrywa, kamien bije nozyczki";
+		}
+	}
+
+	else if (typeid(gracz1) == typeid(Papier)) {
+		if (typeid(gracz2) == typeid(Papier)) {
+			std::cout << " Remis";
+		}
+		else if (typeid(gracz2) == typeid(Kamien)) {
+			std::cout << " Gracz 1 wygrywa, papier bije kamien";
+		}
+		else if (typeid(gracz2) == typeid(Nozyczki)) {
+			std::cout << " Gracz 2 wygrywa, nozyczki bija papier";
+		}
+	}
+
+	else if (typeid(gracz1) == typeid(Nozyczki)) {
+		if (typeid(gracz2) == typeid(Nozyczki)) {
+			std::cout << " Remis";
+		}
+		else if (typeid(gracz2) == typeid(Papier)) {
+			std::cout << " Gracz 1 wygrywa, nozyczki bija papier";
+		}
+		else if (typeid(gracz2) == typeid(Kamien)) {
+			std::cout << " Gracz 2 wygrywa, kamien bije norzyczki";
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
